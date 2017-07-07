b47e98df5072b043c1797659723b086fb1159dc7
syukur91 <syukur.jtk09@gmail.com>
2017-07-07 21:51:46 +0700

    add auto swipe

--- app/src/main/java/com/example/syukur/imagesliderdemo/MainActivity.java
+++ app/src/main/java/com/example/syukur/imagesliderdemo/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.syukur.imagesliderdemo;
 
+import android.os.Handler;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
     ViewPager viewPager;
     CustomAdapter adapter;
+    int currentPage = 0;
+    Timer timer;
+    final long DELAY_MS = 500;//delay in milliseconds before task is to be executed
+    final long PERIOD_MS = 3000; // time in milliseconds between successive task executions.
+    final long NUM_PAGES = 4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +24,26 @@ public class MainActivity extends AppCompatActivity {
         viewPager = (ViewPager)findViewById(R.id.view_pager);
         adapter = new CustomAdapter(this);
         viewPager.setAdapter(adapter);
+
+        /*After setting the adapter use the timer */
+        final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            public void run() {
+                if (currentPage == NUM_PAGES-1) {
+                    currentPage = 0;
+                }
+                viewPager.setCurrentItem(currentPage++, true);
+            }
+        };
+
+        timer = new Timer(); // This will create a new Thread
+        timer .schedule(new TimerTask() { // task to be scheduled
+
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, 500, 3000);
+
     }
 }
